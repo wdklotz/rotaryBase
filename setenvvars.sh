6496e5df3ccaf7cd30e60b3dc65734d96d65c73d
Wolf-Dieter Klotz <wdklotz@gmail.com>
2017-03-07 09:34:45 +0100

    07.03.2017

---
+++ setenvvars.sh
@@ -0,0 +1,9 @@
+unset PYTHONPATH
+# Setting PATH for Python 2.7
+#..done by custom installation of python 2.7
+# The orginal version is saved in .bash_profile.pysave
+# is /usr/bin/python - so nothing todo
+PATH="/Library/Frameworks/Python.framework/Versions/2.7/bin:${PATH}"
+export PATH
+PYTHONPATH=/Library/Frameworks/Python.framework/Versions/2.7/lib/python27.zip:/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7:/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/plat-darwin:/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/plat-mac:/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/plat-mac/lib-scriptpackages:/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-tk:/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-old:/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/lib-dynload:/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages:/Library/Python/2.7/site-packages
+export PYTHONPATH
